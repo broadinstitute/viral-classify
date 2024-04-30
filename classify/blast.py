8238b8adee2f8be2fd14ec54215f6f713845e425
golu099 <flavianegrete@gmail.com>
2024-04-30 19:07:47 -0400

    max_target_seqs def 1

--- classify/blast.py
+++ classify/blast.py
@@ -128,7 +128,7 @@ class BlastnTool(BlastTools):
             db,
             threads=threads)
 
-    def get_hits_fasta(self, inFasta, db, threads, outfmt, task, max_target_seqs, output_type='read_id'):
+    def get_hits_fasta(self, inFasta, db, threads, outfmt, task, max_target_seqs=1, output_type='read_id'):
         start_time = time.time()
         _log.info(f"Executing get_hits_fasta function. Called with outfmt: {outfmt}")
         with open(inFasta, 'rt') as inf:
