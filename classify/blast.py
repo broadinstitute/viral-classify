c211e41b0c073adf5bf5d37b48ac9c87760e1bbd
golu099 <flavianegrete@gmail.com>
2024-05-01 13:32:05 -0400

    adding logging configurations to outfmt/task values at get hits pipe

--- classify/blast.py
+++ classify/blast.py
@@ -91,7 +91,8 @@ class BlastnTool(BlastTools):
         ]
         cmd = [str(x) for x in cmd]
         #Log BLAST command executed
-        _log.info(f"Executing get_hits_pipe function. Called with task: {task}")
+        _log.info(f"After executing get_hits_pipe function. Called with outfmt: {outfmt}")
+        _log.info(f"Executing get_hits_pipe function. Called with task: {task} ,type: {type(task)}")
         _log.info('Running blastn command: {}'.format(' '.join(cmd)))
         
         #try/finally block added to ensure resource packages are cleaned up regardless of error raised
